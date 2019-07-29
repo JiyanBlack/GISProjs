5410b19139399568d4ed8a507bbeeb0a1b87bfbf
JiyanBlack <rglsm7655558@gmail.com>
2019-07-29 13:21:57 +0800

    viz count data

---
+++ connectMlinks/viz/index.js
@@ -0,0 +1,15 @@
+
+
+var mymap = L.map('mapid').setView([-31.948452, 115.857375], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+}).addTo(mymap);
+
+Object.keys(mydata).forEach(key => {
+    L.marker(mydata[key].loc).addTo(mymap).bindPopup(mydata[key].desc);
+})
