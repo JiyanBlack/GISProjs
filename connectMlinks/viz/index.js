9aa2785cb85bb4cbbbf5505f6002acbbcddaf299
JiyanBlack <rglsm7655558@gmail.com>
2019-08-07 11:44:31 +0800

    FTT to smooth curve

--- connectMlinks/viz/index.js
+++ connectMlinks/viz/index.js
@@ -1,6 +1,6 @@
 
 
-var mymap = L.map('mapid').setView([-31.948452, 115.857375], 13);
+var mymap = L.map('mapid').setView([-31.948452, 115.857375], 14);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
